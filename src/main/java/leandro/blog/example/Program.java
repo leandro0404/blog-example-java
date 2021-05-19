e410d92836d50d3c30397701d641a5cac3377769
Leandro Silveira <leandrobhmgg@gmail.com>
2021-05-18 23:12:10 -0300

    criando endpoint get  para listagem de blog

--- src/main/java/leandro/blog/example/Program.java
+++ src/main/java/leandro/blog/example/Program.java
@@ -1,9 +1,11 @@
-package leandro.blog.example.start;
+package leandro.blog.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class Program {
     public static void main ( String[]args)
     {
--- src/main/java/leandro/blog/example/domain/Blog.java
+++ src/main/java/leandro/blog/example/domain/Blog.java
@@ -3,8 +3,25 @@ package leandro.blog.example.domain;
 public class Blog {
     private String title;
     private String description;
-    public Blog ()
-    {
 
+    public Blog(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
